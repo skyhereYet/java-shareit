e7bf3e390ee5cc5bff8d4dff9314193458b81d37
SkyhereYet <mr.mvpopov@yandex.ru>
2023-11-25 15:55:19 +0300

    Feat: добавить функцию бронирования и комментирования

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceDao.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceDao.java
@@ -78,7 +78,7 @@ public class ItemServiceDao implements ItemService {
         List<Booking> bookingList = bookingRepository.findAllByItemOwnerStartDesc(userId);
         List<Comment> comments = new ArrayList<>();//commentRepository.findCommentByAuthorOrderByCreated()
         return items.stream()
-                .sorted(Comparator.comparingInt(Item :: getId))
+                .sorted(Comparator.comparingInt(Item::getId))
                 .map(i -> ItemMapper.toItemInfoDto(i, bookingList, comments))
                 .collect(Collectors.toList());
     }
@@ -103,7 +103,7 @@ public class ItemServiceDao implements ItemService {
         text = "%" + text + "%";
         return itemRepository.findItemsByRequest(text)
                 .stream()
-                .map(ItemMapper :: toItemDto)
+                .map(ItemMapper::toItemDto)
                 .collect(Collectors.toList());
     }
 
