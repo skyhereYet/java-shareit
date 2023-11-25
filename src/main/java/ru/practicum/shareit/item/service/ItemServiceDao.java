bb47189ca949851fc07045f56a12dcb67555bac5
SkyhereYet <mr.mvpopov@yandex.ru>
2023-11-25 15:41:58 +0300

    Feat: добавить функцию бронирования и комментирования

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceDao.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceDao.java
@@ -78,7 +78,7 @@ public class ItemServiceDao implements ItemService {
         List<Booking> bookingList = bookingRepository.findAllByItemOwnerStartDesc(userId);
         List<Comment> comments = new ArrayList<>();//commentRepository.findCommentByAuthorOrderByCreated()
         return items.stream()
-                .sorted(Comparator.comparingInt(Item ::getId))
+                .sorted(Comparator.comparingInt(Item :: getId))
                 .map(i -> ItemMapper.toItemInfoDto(i, bookingList, comments))
                 .collect(Collectors.toList());
     }
@@ -116,11 +116,9 @@ public class ItemServiceDao implements ItemService {
                 .orElseThrow(() -> new UserExistException("User not exist in the repository, ID = "  + userId));
         List<Booking> bookingList = bookingRepository
                 .findAllByItemAndBookerAndEndBeforeOrderByStart(userId, itemId, LocalDateTime.now());
-        boolean bookerExist = false;
         if (!bookingList.isEmpty()) {
             for (Booking booker : bookingList) {
                 if (booker.getEnd().isBefore(LocalDateTime.now())) {
-                    bookerExist = true;
                     break;
                 }
             }
